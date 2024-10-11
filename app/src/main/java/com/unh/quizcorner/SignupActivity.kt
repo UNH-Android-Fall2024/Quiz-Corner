588b1a4e4c506527c398ae27d8a534bfd02f7b22
Jayavardhan21 <schin39@unh.newhaven.edu>
2024-10-10 21:58:56 -0700

    Designed Login/ SignUp pages

---
+++ app/src/main/java/com/unh/quizcorner/SignupActivity.kt
@@ -0,0 +1,17 @@
+package com.unh.quizcorner
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignupActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+    }
+}
