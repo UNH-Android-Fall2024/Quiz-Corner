209b0aee7c83094124f0fcbf74687a4c187927c7
Jayavardhan21 <schin39@unh.newhaven.edu>
2024-10-13 21:38:02 -0700

    image, Linear layout designed in QuizMainActivity

--- app/src/main/java/com/unh/quizcorner/HomeFragment.kt
+++ app/src/main/java/com/unh/quizcorner/HomeFragment.kt
@@ -1,20 +1,54 @@
 package com.unh.quizcorner
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.unh.quizcorner.databinding.FragmentHomeBinding
+//
+//class HomeFragment : Fragment() {
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_home, container, false)
+//
+//    }
+//}
+
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout using view binding
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up the button click listener to navigate to QuizMainActivity
+        binding.navigateQuizButton.setOnClickListener {
+            val intent = Intent(requireActivity(), QuizMainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
+
