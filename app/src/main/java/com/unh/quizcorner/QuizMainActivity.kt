2c8a12993353f7fdd2e9384c87d478715e5d4678
Jayavardhan21 <schin39@unh.newhaven.edu>
2024-10-27 11:51:03 -0400

    firebase firestore connected

--- app/src/main/java/com/unh/quizcorner/QuizMainActivity.kt
+++ app/src/main/java/com/unh/quizcorner/QuizMainActivity.kt
@@ -1,6 +1,7 @@
 package com.unh.quizcorner
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.widget.Toolbar
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import com.unh.quizcorner.databinding.ActivityMainBinding
 import com.unh.quizcorner.databinding.ActivityQuizMainBinding
 
@@ -29,26 +32,73 @@ class QuizMainActivity : AppCompatActivity() {
 
     }
 
+//    private fun setupRecyclerview(){
+//        adapter = QuizListAdapter(quizModelList)
+//        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+//        binding.recyclerView.adapter = adapter
+//    }
+
+//    private fun getDataFromFirebase(){
+//        // dummy data
+//
+//        val listQuestionModel = mutableListOf<QuestionModel>()
+//        listQuestionModel.add(QuestionModel("What is Android ? ", mutableListOf("language","OS","Product","None"), correct = "OS"))
+//        listQuestionModel.add(QuestionModel("Who owns  Android ? ", mutableListOf("Apple","MS","Kotlin","Google"), correct = "Google"))
+//        listQuestionModel.add(QuestionModel("Who owns  Android ? ", mutableListOf("Apple","MS","Kotlin","Google"), correct = "Google"))
+//
+//
+//        quizModelList.add(QuizModel("1","Programming","Basic programming","10",listQuestionModel))
+//        setupRecyclerview()
+//    }
+
+    private fun getDataFromFirebase(){
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("quizzes").get()
+            .addOnSuccessListener { quizDocuments ->
+                for (quizDocument in quizDocuments) {
+                    val quizId = quizDocument.id
+                    val title = quizDocument.getString("title") ?: ""
+                    val subtitle = quizDocument.getString("subtitle") ?: ""
+                    val time = quizDocument.getString("time") ?: ""
+
+                    val questionList = mutableListOf<QuestionModel>()
+
+                    db.collection("quizzes").document(quizId).collection("questions").get()
+                        .addOnSuccessListener { questionDocuments ->
+                            for (questionDocument in questionDocuments) {
+                                val question = questionDocument.getString("question") ?: ""
+                                val options = questionDocument.get("options") as? List<String> ?: listOf()
+                                val correct = questionDocument.getString("correct") ?: ""
+
+                                val questionModel = QuestionModel(question, options, correct)
+                                questionList.add(questionModel)
+                            }
+
+                            val quizModel = QuizModel(quizId, title, subtitle, time, questionList)
+                            quizModelList.add(quizModel)
+
+                            // After data is added, check if adapter is initialized
+                            if (!::adapter.isInitialized) {
+                                setupRecyclerview() // Only initialize adapter once
+                            } else {
+                                adapter.notifyDataSetChanged() // Refresh adapter if already set
+                            }
+                        }
+                        .addOnFailureListener { e ->
+                            Log.e("Firestore", "Error fetching questions for quiz $quizId: ", e)
+                        }
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e("Firestore", "Error fetching quizzes: ", e)
+            }
+    }
+
     private fun setupRecyclerview(){
         adapter = QuizListAdapter(quizModelList)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
-    }
-
-    private fun getDataFromFirebase(){
-        // dummy data
-
-        val listQuestionModel = mutableListOf<QuestionModel>()
-        listQuestionModel.add(QuestionModel("What is Android ? ", mutableListOf("language","OS","Product","None"), correct = "OS"))
-        listQuestionModel.add(QuestionModel("Who owns  Android ? ", mutableListOf("Apple","MS","Kotlin","Google"), correct = "Google"))
-
-        listQuestionModel.add(QuestionModel("Who owns  Android ? ", mutableListOf("Apple","MS","Kotlin","Google"), correct = "Google"))
-
-
-        quizModelList.add(QuizModel("1","Programming","Basic programming","10",listQuestionModel))
-//        quizModelList.add(QuizModel("2","Science","Science topics","10"))
-//        quizModelList.add(QuizModel("3","History","Topics of History","10"))
-        setupRecyclerview()
     }
 
 }
